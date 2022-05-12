cc93e598fabfb4900498c0048312c6aa7111a104
coderrakibul <coder.rakibul@gmail.com>
2022-05-12 13:27:39 +0600

    button updated

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -4,8 +4,8 @@ const Service = ({ service, setTreatment }) => {
     const { name, slots } = service;
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-            <div class="card-body">
-                <h2 class="card-title text-secondary">{name}</h2>
+            <div class="card-body text-center">
+                <h2 class="text-xl font-bold text-secondary">{name}</h2>
                 <p>{
                     slots.length > 0
                         ? <span>{slots[0]}</span>
@@ -13,7 +13,7 @@ const Service = ({ service, setTreatment }) => {
                 }</p>
                 <p>  {slots.length} {slots.lenght > 1 ? "spaces" : "space"} available</p>
                 <div class="card-actions justify-center">
-                    <label for="booking-modal" onClick={() => setTreatment(service)} disabled={slots.length === 0} class="btn bg-gradient-to-r from-secondary to-primary text-white uppercase border-0">Book Appointment</label>
+                    <label for="booking-modal" onClick={() => setTreatment(service)} disabled={slots.length === 0} class="btn btn-sm bg-gradient-to-r from-secondary to-primary text-white uppercase border-0">Book Appointment</label>
                 </div>
             </div>
         </div>
