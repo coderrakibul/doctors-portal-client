3e80d54c07ec4107bd2e7be17620946479a19caf
coderrakibul <coder.rakibul@gmail.com>
2022-05-12 15:11:07 +0600

    google sign in added

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -2,12 +2,12 @@ import chair from '../../assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 
-const AppointmentBanner = ({date, setDate}) => {
+const AppointmentBanner = ({ date, setDate }) => {
 
     return (
-        <div class="hero min-h-screen">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt="dentist chair" />
+        <div className="hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt="dentist chair" />
                 <div>
                     <DayPicker
                         mode="single"
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -3,17 +3,17 @@ import React from 'react';
 const Service = ({ service, setTreatment }) => {
     const { name, slots } = service;
     return (
-        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-            <div class="card-body text-center">
-                <h2 class="text-xl font-bold text-secondary">{name}</h2>
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+            <div className="card-body text-center">
+                <h2 className="text-xl font-bold text-secondary">{name}</h2>
                 <p>{
                     slots.length > 0
                         ? <span>{slots[0]}</span>
                         : <span className='text-red-500'>Try Another Date</span>
                 }</p>
                 <p>  {slots.length} {slots.lenght > 1 ? "spaces" : "space"} available</p>
-                <div class="card-actions justify-center">
-                    <label for="booking-modal" onClick={() => setTreatment(service)} disabled={slots.length === 0} class="btn btn-sm bg-gradient-to-r from-secondary to-primary text-white uppercase border-0">Book Appointment</label>
+                <div className="card-actions justify-center">
+                    <label for="booking-modal" onClick={() => setTreatment(service)} disabled={slots.length === 0} className="btn btn-sm bg-gradient-to-r from-secondary to-primary text-white uppercase border-0">Book Appointment</label>
                 </div>
             </div>
         </div>
