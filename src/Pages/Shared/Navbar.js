96d50156dd70db2176b1bf2f1050233df9b8967d
coderrakibul <coder.rakibul@gmail.com>
2022-05-12 11:28:08 +0600

    navbar update

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const menuItems = <>
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/home">Home</Link></li>
         <li><Link to="/appointment">Appointment</Link></li>
         <li><Link to="/review">Review</Link></li>
         <li><Link to="/contact">Contact</Link></li>
