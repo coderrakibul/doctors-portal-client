98e88d20cc57a00009f7f52e605b2378c47c622a
coderrakibul <coder.rakibul@gmail.com>
2022-05-13 12:15:05 +0600

    modal updated

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { format } from 'date-fns';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const BookingModal = ({ date, treatment, setTreatment }) => {
     const { name, slots } = treatment;
+    const [user, loading, error] = useAuthState(auth);
 
     const handleBooking = event => {
         event.preventDefault();
@@ -19,14 +22,14 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
                     <h3 className="font-bold text-lg text-secondary">Booking for: {name}</h3>
 
                     <form onSubmit={handleBooking} className='grid grid-cols-1 gap-2 justify-items-center mt-2'>
-                        <input type="text" readOnly value={format(date, 'PP')} className="input input-bordered w-full max-w-xs" />
+                        <input type="text" disabled value={format(date, 'PP')} className="input input-bordered w-full max-w-xs" />
                         <select name="slot" className="select select-bordered w-full max-w-xs">
                             {
                                 slots.map(slot => <option value={slot}>{slot}</option>)
                             }
                         </select>
-                        <input type="text" name="name" placeholder="Your Name" className="input input-bordered w-full max-w-xs" />
-                        <input type="email" name="email" placeholder="Your Email" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name="name" disabled value={user?.displayName} className="input input-bordered w-full max-w-xs" />
+                        <input type="email" name="email" disabled value={user?.email} className="input input-bordered w-full max-w-xs" />
                         <input type="text" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
                         <input type="submit" value="Submit" className="btn btn-secondary w-full max-w-xs" />
                     </form>
