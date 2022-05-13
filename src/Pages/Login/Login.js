c9af3148a8ba23c78b59bcb5cd6cd605fdc9dd76
coderrakibul <coder.rakibul@gmail.com>
2022-05-13 10:45:13 +0600

    loading spinner added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
+import Loading from '../Shared/Loading';
 
 const Login = () => {
-    const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
 
-    if (user) {
-        console.log(user)
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    let signInError;
+
+    if (loading || gLoading) {
+        return <Loading></Loading>
+    }
+
+    if (error || gError) {
+        signInError = <p className='text-red-500'>{error?.message} || {gError?.message}</p>
+    }
+
+    if (gUser) {
+        console.log(gUser)
     }
 
     const onSubmit = data => {
-        console.log(data)
+        console.log(data);
+        signInWithEmailAndPassword(data.email, data.password);
     };
 
     return (
@@ -71,9 +90,9 @@ const Login = () => {
                             </label>
                         </div>
 
+                        {signInError}
 
-
-                        <input className='btn w-full max-w-xs text-white' type="submit" value="Login"/>
+                        <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
                     </form>
 
                     <div className="divider">OR</div>
