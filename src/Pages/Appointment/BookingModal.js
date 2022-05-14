1e5da17922c886f4ee92ec2b95b5aa709ce8d540
unknown <coder.rakibul@gmail.com>
2022-05-14 23:33:04 +0600

    token addded

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -4,11 +4,10 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { toast } from 'react-toastify';
 
-const BookingModal = ({ date, treatment, setTreatment, refetch }) => {
+const BookingModal = ({ date, treatment, setTreatment }) => {
     const { _id, name, slots } = treatment;
     const [user, loading, error] = useAuthState(auth);
     const formattedDate = format(date, 'PP');
-
     const handleBooking = event => {
         event.preventDefault();
         const slot = event.target.slot.value;
@@ -20,10 +19,10 @@ const BookingModal = ({ date, treatment, setTreatment, refetch }) => {
             slot,
             patient: user.email,
             patientName: user.displayName,
-            phone: event.target.phone.value,
+            phone: event.target.phone.value
         }
 
-        fetch("http://localhost:5000/booking", {
+        fetch('http://localhost:5000/booking', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
@@ -32,19 +31,16 @@ const BookingModal = ({ date, treatment, setTreatment, refetch }) => {
         })
             .then(res => res.json())
             .then(data => {
-
                 if (data.success) {
                     toast(`Appointment is set, ${formattedDate} at ${slot}`)
                 }
                 else {
-                    toast.error(`Already have Appointment on, ${data.booking?.date} at ${data.booking?.slot}`)
+                    toast.error(`Already have and appointment on ${data.booking?.date} at ${data.booking?.slot}`)
                 }
-                refetch();
-                //to close the modal
                 setTreatment(null);
-            })
-
+            });
     }
+
     return (
         <div>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -52,8 +48,7 @@ const BookingModal = ({ date, treatment, setTreatment, refetch }) => {
                 <div className="modal-box">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="font-bold text-lg text-secondary">Booking for: {name}</h3>
-
-                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-2 justify-items-center mt-2'>
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 justify-items-center mt-2'>
                         <input type="text" disabled value={format(date, 'PP')} className="input input-bordered w-full max-w-xs" />
                         <select name="slot" className="select select-bordered w-full max-w-xs">
                             {
