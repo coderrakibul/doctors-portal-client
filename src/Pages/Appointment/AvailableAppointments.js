ca1dc3f9d42080043f67efc8861f017be37ef5e9
unknown <coder.rakibul@gmail.com>
2022-05-14 18:25:05 +0600

    booking modal updated

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,27 +1,38 @@
 import { format } from 'date-fns';
+import id from 'date-fns/esm/locale/id/index.js';
 import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import Service from '../Appointment/Service';
 import BookingModal from './BookingModal';
+import Loading from '../Shared/Loading'
 
 const AvailableAppointments = ({ date }) => {
-    const [services, setServices] = useState([]);
+    // const [services, setServices] = useState([]);
     const [treatment, setTreatment] = useState(null);
-
     const formattedDate = format(date, 'PP');
 
-    useEffect(() => {
-        fetch(`http://localhost:5000/available?date=${formattedDate}`)
-            .then(res => res.json())
-            .then(data => setServices(data));
+    const { data: services, isLoading, refetch } = useQuery(['available', formattedDate], () => fetch(`http://localhost:5000/available?date=${formattedDate}`)
+        .then(res => res.json()))
 
-    }, [])
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
+    // here used react query to reload and cash data and update
+    // useEffect(() => {
+    //     fetch(`http://localhost:5000/available?date=${formattedDate}`)
+    //         .then(res => res.json())
+    //         .then(data => setServices(data));
+
+    // }, [formattedDate])
+
     return (
         <div>
             <h4 className='text-xl text-secondary text-center'> Available Appointment on {format(date, 'PP')} </h4>
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    services.map(service => <Service
+                    services?.map(service => <Service
                         key={service._id}
                         service={service}
                         setTreatment={setTreatment}
@@ -32,6 +43,7 @@ const AvailableAppointments = ({ date }) => {
                 date={date}
                 treatment={treatment}
                 setTreatment={setTreatment}
+                refetch={refetch}
             ></BookingModal>}
         </div>
     );
