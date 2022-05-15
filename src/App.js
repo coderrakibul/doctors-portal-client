53d89c11dbda1a0026822eb10a229e5268a79c61
unknown <coder.rakibul@gmail.com>
2022-05-15 14:30:45 +0600

    app updated

--- src/App.js
+++ src/App.js
@@ -1,49 +1,43 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import About from './Pages/About/About';
-import Appointment from './Pages/Appointment/Appointment';
-import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login';
-import RequireAuth from './Pages/Login/RequireAuth';
-import SignUp from './Pages/Login/SignUp';
 import Navbar from './Pages/Shared/Navbar';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
+import Appointment from './Pages/Appointment/Appointment';
+import SignUp from './Pages/Login/SignUp';
+import RequireAuth from './Pages/Login/RequireAuth';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Dashboard from './Pages/Dashboard/Dashboard';
 import MyAppointment from './Pages/Dashboard/MyAppointment';
 import MyReview from './Pages/Dashboard/MyReview';
-import Review from './Pages/Home/Review';
-import Contact from './Pages/Home/Contact';
 import MyHistory from './Pages/Dashboard/MyHistory';
 import Users from './Pages/Dashboard/Users';
 import RequireAdmin from './Pages/Login/RequireAdmin';
-
+import Review from './Pages/Home/Review';
 
 function App() {
   return (
-    <div className='max-w-7xl mx-auto'>
+    <div className='max-w-7xl mx-auto px-12'>
       <Navbar></Navbar>
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/home" element={<Home></Home>}></Route>
-        <Route path="/appointment" element={<RequireAuth>
-          <Appointment></Appointment>
-        </RequireAuth>}></Route>
-        <Route path="/dashboard" element={<RequireAuth>
-          <Dashboard></Dashboard>
-        </RequireAuth>}>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="home" element={<Home></Home>} />
+        <Route path="about" element={<About></About>} />
+        <Route path="appointment" element={
+          <RequireAuth>
+            <Appointment></Appointment>
+          </RequireAuth>
+        } />
+        <Route path="dashboard" element={<RequireAuth><Dashboard></Dashboard></RequireAuth>} >
           <Route index element={<MyAppointment></MyAppointment>}></Route>
           <Route path="review" element={<MyReview></MyReview>}></Route>
           <Route path="history" element={<MyHistory></MyHistory>}></Route>
-          <Route path="users" element={<RequireAdmin>
-            <Users></Users>
-          </RequireAdmin>}></Route>
+          <Route path="users" element={<RequireAdmin><Users></Users></RequireAdmin>}></Route>
         </Route>
-        <Route path="/review" element={<Review></Review>}></Route>
-        <Route path="/contact" element={<Contact></Contact>}></Route>
-        <Route path="/about" element={<About></About>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+        <Route path="login" element={<Login></Login>} />
+        <Route path="signup" element={<SignUp></SignUp>} />
       </Routes>
       <ToastContainer />
     </div>
